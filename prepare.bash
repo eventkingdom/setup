3b1858c9a2702e1d927f9c3885aafc6f4e65f219
eventkingdom <154554551+eventkingdom@users.noreply.github.com>
2023-12-21 21:24:32 +0100

    Update prepare.bash

--- prepare.bash
+++ prepare.bash
@@ -2,3 +2,14 @@
 
 mkdir -p ~/EK/Event.Kingdom.Mail
 cd ~/EK
+
+# to enforce 'ekmaster' user (e.g. when system user is not 'ekmaster') use 'svn co --username ekmaster ..' syntax
+# or during checkout enter empty password for current user - then you will be asked for user again
+svn co --username ekmaster ${SVNROOT}/Event.Kingdom.Mail ./Event.Kingdom.Mail
+
+mkdir -p ~/collected-mails
+mkdir -p ~/processed
+
+cp -f ~/EK/Event.Kingdom.Mail/etc/env/common/home/_common/bin/update ~/bin/
+chmod a+x ~/bin/update
+~/bin/update
