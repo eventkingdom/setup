df53331ae285b7aaf856006930d2d31a15449774
eventkingdom <154554551+eventkingdom@users.noreply.github.com>
2024-01-05 22:23:15 +0100

    Update bootstrap_ba.bash

--- bootstrap_ba.bash
+++ bootstrap_ba.bash
@@ -5,9 +5,12 @@
 # subversion needs to be installed already
 ####
 
+mkdir ~/bin
 mkdir -p ~/EK/Event.Kingdom.Mail
 mkdir -p ~/EK/Event.Kingdom.Translation
 
 cd ~/EK
 svn co --username ekbaupdater ${SVNROOT}/Event.Kingdom.Mail ./Event.Kingdom.Mail
 svn co --username ekbaupdater ${SVNROOT}/Event.Kingdom.Translation ./Event.Kingdom.Translation
+
+cp -f ~/EK/Event.Kingdom.Mail/etc/env/common/home/_common/bin/update.bash ~/bin/
